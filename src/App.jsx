3613e74ac71286979e0f7437f91ae32135d165db
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-03-03 00:21:41 +0200

    add bonus tip

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,33 @@ function App() {
             <p>this is a new paragraph just to check if it works</p>
             <p>last test</p>
             <p>feature-1</p>
+
+            <h1>BONUS TIP</h1>
             <p>
-                1.create branch -go to main: git checkout main -create branch:
-                git branch feature-1 -list all branches: git branch -a -go to
-                the new branch: git checkout feature-1 or simply create a branch
-                and open it at once: -git checkout -b [branch_name] 2.stage and
-                commit changes on branch -from the branch do the following
-                -stage changes: git add . -commit changes: git commit -am "added
-                feature-1" 3.merge branches with main -git checkout main -git
-                merge feature-1 4.delete branch -go to main: git checkout main
-                -git branch -d feature-1 (if branch is merged) -git branch -D
-                feature-1 (if branch is not merged)
+                1.create branch <br />
+                -go to main: git checkout main <br />
+                -create branch: git branch feature-1 <br />
+                -list all branches: git branch -a <br />
+                -go to the new branch: git checkout feature-1 <br />
+                or simply create a branch and open it at once: -git checkout -b
+                [branch_name]
+            </p>{" "}
+            <p>
+                2.stage and commit changes on branch <br />
+                -from the branch do the following <br />
+                -stage changes: git add . <br />
+                -commit changes: git commit -am "added feature-1"
+            </p>{" "}
+            <p>
+                3.merge branches with main <br />
+                -git checkout main <br />
+                -git merge feature-1
+            </p>{" "}
+            <p>
+                4.delete branch <br />
+                -go to main: git checkout main <br />
+                -git branch -d feature-1 (if branch is merged) <br />
+                -git branch -D feature-1 (if branch is not merged)
             </p>
         </>
     );
