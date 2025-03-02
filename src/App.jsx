3b4262b72d55eb2c1ad4d8ccc8c1b4ca99d2ba66
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-03-03 00:00:28 +0200

    added feature-1

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
             <p>A place for 3d printable models</p>
             <p>this is a new paragraph just to check if it works</p>
             <p>last test</p>
+            <p>feature-1</p>
         </>
     )
 }
