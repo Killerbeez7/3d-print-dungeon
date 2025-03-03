4ce0e7e6bbf5bc93c53c86eec306e3b731865156
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-03-04 00:38:05 +0200

    add react router, add navbar/footer, tailwind ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,24 @@
-function App() {
-    return (
-        <>
-            <h1>3D Print Dungon</h1>
-            <p>A place for 3d printable models</p>
-            <p>this is a new paragraph just to check if it works</p>
-            <p>last test</p>
-            <p>feature-1</p>
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-            <h1>BONUS TIP</h1>
-            <p>
-                1.create branch <br />
-                -go to main: git checkout main <br />
-                -create branch: git branch feature-1 <br />
-                -list all branches: git branch -a <br />
-                -go to the new branch: git checkout feature-1 <br />
-                or simply create a branch and open it at once: -git checkout -b
-                [branch_name]
-            </p>{" "}
-            <p>
-                2.stage and commit changes on branch <br />
-                -from the branch do the following <br />
-                -stage changes: git add . <br />
-                -commit changes: git commit -am "added feature-1"
-            </p>{" "}
-            <p>
-                3.merge branches with main <br />
-                -git checkout main <br />
-                -git merge feature-1
-            </p>{" "}
-            <p>
-                4.delete branch <br />
-                -go to main: git checkout main <br />
-                -git branch -d feature-1 (if branch is merged) <br />
-                -git branch -D feature-1 (if branch is not merged)
-            </p>
-        </>
+// components
+import { Navbar } from "./components/shared/navbar/Navbar";
+import { Footer } from "./components/shared/footer/Footer";
+import { Home } from "./components/home/Home";
+
+export default function App() {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Navbar />
+
+            <main className="flex-grow px-4 py-4">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+                <h1>hello</h1>
+            </main>
+
+            <Footer />
+        </div>
     );
 }
-
-export default App;
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Home = () => {
+    return (
+        <section style={{ padding: "1rem" }}>
+            <h1>Welcome to the Home Page</h1>
+            <p>
+                This is where you can highlight your main features or message.
+            </p>
+        </section>
+    );
+};
