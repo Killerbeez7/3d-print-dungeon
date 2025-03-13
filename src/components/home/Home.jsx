6b7a41ced31cf7832811081c094ea66c12bb6b09
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-03-13 15:15:45 +0200

    .

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,13 @@
 export const Home = () => {
     return (
         <section style={{ padding: "1rem" }}>
-            <h1>Welcome to the Home Page</h1>
-            <p>
-                This is where you can highlight your main features or message.
+            <h1 className="text-4xl ">Today's updates!</h1>
+
+            <p className="text-2xl ">
+                1.Working auth modal for login/signup (front end)
             </p>
+            <p className="text-2xl ">2.working upload page (front end)</p>
+            <p className="text-2xl ">3.feature pages (templates)</p>
         </section>
     );
 };
