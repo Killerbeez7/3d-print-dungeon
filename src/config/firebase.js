f980dedcb63ef6277cdeca00cf91bac93377d009
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-04-09 15:17:36 +0300

    move firebase config file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
@@ -13,10 +13,12 @@ const firebaseConfig = {
     measurementId: "G-LMBWGFK6TF",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+// Initialize services
 export const db = getFirestore(app);
+export const auth = getAuth(app);
 export const storage = getStorage(app);
 
-export { app };
+export { app }; 
