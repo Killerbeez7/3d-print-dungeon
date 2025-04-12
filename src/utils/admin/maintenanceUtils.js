4bd45619baaa0882be25d74e4750b0801c88ccd4
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-04-12 21:36:51 +0300

    create maintenance utils

---
+++ src/utils/admin/maintenanceUtils.js
@@ -0,0 +1,34 @@
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../config/firebase";
+import { isAdmin } from "../../services/adminService";
+
+export const checkMaintenanceMode = async (uid) => {
+    try {
+        // Check maintenance mode from settings
+        const settingsRef = doc(db, "settings", "global");
+        const settingsDoc = await getDoc(settingsRef);
+        
+        if (!settingsDoc.exists()) {
+            return { isMaintenance: false, isAdmin: false };
+        }
+
+        const maintenanceMode = settingsDoc.data().siteMaintenanceMode || false;
+        
+        // If maintenance mode is off, no need to check admin status
+        if (!maintenanceMode) {
+            return { isMaintenance: false, isAdmin: false };
+        }
+
+        // If maintenance mode is on, check if user is admin
+        const adminStatus = uid ? await isAdmin(uid) : false;
+        
+        return {
+            isMaintenance: maintenanceMode,
+            isAdmin: adminStatus
+        };
+    } catch (error) {
+        console.error("Error checking maintenance mode:", error);
+        // In case of error, assume maintenance mode is off
+        return { isMaintenance: false, isAdmin: false };
+    }
+}; 
