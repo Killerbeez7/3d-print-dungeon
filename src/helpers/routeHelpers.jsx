e00adf546083c62c0bc47bb09f4c81542976d94f
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-04-15 17:49:49 +0300

    create routeHelpers

---
+++ src/helpers/routeHelpers.jsx
@@ -0,0 +1,34 @@
+import { MaintenanceRoute } from "../routes/guards/MaintenanceRoute";
+import { ProtectedRoute } from "../routes/guards/ProtectedRoute";
+
+export const withMaintenance = (element, options = {}) => (
+  <MaintenanceRoute 
+    fallback={options.fallback}
+    redirectTo={options.redirectTo}
+  >
+    {element}
+  </MaintenanceRoute>
+);
+
+export const withProtected = (element, options = {}) => (
+  <ProtectedRoute 
+    requireAdmin={options.requireAdmin}
+    redirectTo={options.redirectTo}
+  >
+    {element}
+  </ProtectedRoute>
+);
+
+export const withProtectedMaintenance = (element, options = {}) => (
+  <MaintenanceRoute
+    fallback={options.fallback}
+    redirectTo={options.maintenanceRedirect}
+  >
+    <ProtectedRoute
+      requireAdmin={options.requireAdmin}
+      redirectTo={options.authRedirect}
+    >
+      {element}
+    </ProtectedRoute>
+  </MaintenanceRoute>
+);
