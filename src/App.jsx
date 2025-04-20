857dc77cc6520920b8be7ea66b79be5ba4318e41
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-04-20 18:17:26 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { AuthProvider } from "./contexts/authContext";
 import { ModelsProvider } from "./contexts/modelsContext";
 import { SearchProvider } from "./contexts/searchContext";
 import { ForumProvider } from "./contexts/forumContext";
+
 import { AppRoutes } from "./AppRoutes";
 
 export const App = () => {
