c60411483b95811a8048740707b6ab1d572dc54a
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-04-20 14:57:49 +0300

    update business routes

--- src/routes/businessRoutes.jsx
+++ src/routes/businessRoutes.jsx
@@ -1,26 +1,14 @@
-import { Business } from "../components/business/Business";
 import { BulkOrders } from "../components/business/BulkOrders";
 import { CustomSolutions } from "../components/business/CustomSolutions";
 import { Enterprise } from "../components/business/Enterprise";
 import { withMaintenance } from "../helpers/routeHelpers";
+import { ROUTES } from "../config/routeConstants";
 
 export const businessRoutes = [
+    { path: ROUTES.BUSINESS_BULK_ORDERS, element: withMaintenance(<BulkOrders />) },
     {
-        path: "/business",
-        element: withMaintenance(<Business />),
-        children: [
-            {
-                path: "bulk-orders",
-                element: <BulkOrders />,
-            },
-            {
-                path: "custom-solutions",
-                element: <CustomSolutions />,
-            },
-            {
-                path: "enterprise",
-                element: <Enterprise />,
-            },
-        ],
+        path: ROUTES.BUSINESS_CUSTOM_SOLUTIONS,
+        element: withMaintenance(<CustomSolutions />),
     },
+    { path: ROUTES.BUSINESS_ENTERPRISE_SUITE, element: withMaintenance(<Enterprise />) },
 ];
