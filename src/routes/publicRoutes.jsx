003914a211253958f376a6162ff5a29558b445d0
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-04-20 14:56:26 +0300

    update public routes

--- src/routes/publicRoutes.jsx
+++ src/routes/publicRoutes.jsx
@@ -2,18 +2,10 @@ import { Home } from "../components/home/Home";
 import { MaintenancePage } from "../components/shared/MaintenancePage";
 import { DynamicSearch } from "../components/search/DynamicSearch";
 import { withMaintenance } from "../helpers/routeHelpers";
+import { ROUTES } from "../config/routeConstants";
 
 export const publicRoutes = [
-    {
-        path: "/maintenance",
-        element: <MaintenancePage />,
-    },
-    {
-        path: "/",
-        element: withMaintenance(<Home />),
-    },
-    {
-        path: "/search",
-        element: withMaintenance(<DynamicSearch />),
-    },
+    { path: ROUTES.HOME, element: withMaintenance(<Home />) },
+    { path: ROUTES.SEARCH, element: withMaintenance(<DynamicSearch />) },
+    { path: ROUTES.MAINTENANCE, element: <MaintenancePage /> },
 ];
