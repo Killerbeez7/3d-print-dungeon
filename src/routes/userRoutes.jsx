01b29a41190f1ef2559dd6a960ab5a7ca844345c
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-04-20 14:56:01 +0300

    update user routes

--- src/routes/userRoutes.jsx
+++ src/routes/userRoutes.jsx
@@ -2,18 +2,10 @@ import { ProfileSettings } from "../components/settings/ProfileSettings";
 import { ArtistProfile } from "../components/artists/artist-profile/ArtistProfile";
 import { ArtistsList } from "../components/artists/ArtistsList";
 import { withMaintenance, withProtectedMaintenance } from "../helpers/routeHelpers";
+import { ROUTES } from "../config/routeConstants";
 
 export const userRoutes = [
-    {
-        path: "/settings",
-        element: withProtectedMaintenance(<ProfileSettings />),
-    },
-    {
-        path: "/artists",
-        element: withMaintenance(<ArtistsList />),
-    },
-    {
-        path: "/artist/:id",
-        element: withMaintenance(<ArtistProfile />),
-    },
+    { path: ROUTES.SETTINGS, element: withProtectedMaintenance(<ProfileSettings />) },
+    { path: ROUTES.USER_ARTISTS, element: withMaintenance(<ArtistsList />) },
+    { path: ROUTES.USER_ARTIST_PROFILE, element: withMaintenance(<ArtistProfile />) },
 ];
