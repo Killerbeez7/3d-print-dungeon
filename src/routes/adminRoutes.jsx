b583906688705824d76ddf1fbfcd9323c1db68df
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-04-20 14:58:01 +0300

    update admin routes

--- src/routes/adminRoutes.jsx
+++ src/routes/adminRoutes.jsx
@@ -1,9 +1,10 @@
 import { AdminPanel } from "../components/admin/AdminPanel";
 import { withProtectedMaintenance } from "../helpers/routeHelpers";
+import { ROUTES } from "../config/routeConstants";
 
 export const adminRoutes = [
     {
-        path: "/admin-panel",
+        path: ROUTES.ADMIN_PANEL,
         element: withProtectedMaintenance(<AdminPanel />, {
             requireAdmin: true,
             authRedirect: "/login",
