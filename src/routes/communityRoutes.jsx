2f459726677d11de4abea9c8772cc9f1b71078e9
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-05-03 15:06:35 +0300

    add new routes and refactor old ones

--- src/routes/communityRoutes.jsx
+++ src/routes/communityRoutes.jsx
@@ -1,10 +1,9 @@
-import { Home } from "../components/home/Home";
-import { Events } from "../components/community/Events";
-import { Blog } from "../components/community/Blog";
+import { EventsHome } from "../components/events/EventsHome";
+import { Blog } from "../components/blog/Blog";
 import { withMaintenance } from "../helpers/routeHelpers";
 import { ROUTES } from "../config/routeConstants";
 
 export const communityRoutes = [
-    { path: ROUTES.COMMUNITY_EVENTS, element: withMaintenance(<Events />) },
-    { path: ROUTES.COMMUNITY_BLOG, element: withMaintenance(<Blog />) },
+    { path: ROUTES.EVENTS, element: withMaintenance(<EventsHome />) },
+    { path: ROUTES.BLOG, element: withMaintenance(<Blog />) },
 ];
