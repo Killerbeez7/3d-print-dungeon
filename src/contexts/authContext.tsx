f8519a11058a67402578fb31aa3ab809e470f3a5
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-05-25 15:33:48 +0300

    move types into separate file and add roles array in the constext

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -1,30 +1,10 @@
-import { User } from "firebase/auth";
 import { createContext } from "react";
-import { UserData } from "../types/auth";
-
-// Types for AuthContext
-interface AuthContextType {
-    currentUser: User | null;
-    userData: UserData | null;
-    authError: string;
-    maintenanceMode: boolean;
-    maintenanceMessage: string | null;
-    maintenanceEndTime: Date | null;
-    isAdmin: boolean;
-    loading: boolean;
-    handleEmailSignUp: (email: string, password: string) => Promise<void>;
-    handleEmailSignIn: (email: string, password: string) => Promise<void>;
-    handleGoogleSignIn: () => Promise<void>;
-    handleGithubSignIn: () => Promise<void>;
-    handleSignOut: () => Promise<void>;
-    changePassword: (currentPassword: string, newPassword: string) => Promise<void>;
-    fetchUserData: () => Promise<void>;
-    handleAuthError: (error: unknown, provider: string) => never;
-}
+import { AuthContextType } from "../types/authContext";
 
 export const AuthContext = createContext<AuthContextType>({
     currentUser: null,
     userData: null,
+    roles: [],
     authError: "",
     maintenanceMode: false,
     maintenanceMessage: null,
