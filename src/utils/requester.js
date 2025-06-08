93823e038f099167e8a6a72b4df098fd813a10cc
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-06-09 00:14:59 +0300

    #

--- src/utils/requester.js
+++ src/utils/requester.js
@@ -1,49 +1,49 @@
-async function requester(method, url, data) {
-    const options = {};
+// async function requester(method, url, data) {
+//     const options = {};
 
-    // TODO: Implement logic for access token
+//     // TODO: Implement logic for access token
     
-    // if (accessToken) {
-    //     options.headers = {
-    //         ...options.headers,
-    //         "X-Authorization": accessToken,
-    //     };
-    // }
+//     // if (accessToken) {
+//     //     options.headers = {
+//     //         ...options.headers,
+//     //         "X-Authorization": accessToken,
+//     //     };
+//     // }
 
-    if (method !== "GET") {
-        options.method = method;
-    }
+//     if (method !== "GET") {
+//         options.method = method;
+//     }
 
-    if (data) {
-        options.headers = {
-            ...options.headers,
-            "Content-Type": "application/json",
-        };
+//     if (data) {
+//         options.headers = {
+//             ...options.headers,
+//             "Content-Type": "application/json",
+//         };
 
-        options.body = JSON.stringify(data);
-    }
+//         options.body = JSON.stringify(data);
+//     }
 
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
+//     try {
+//         const response = await fetch(url, options);
+//         const result = await response.json();
 
-        if (!response.ok) {
-            throw new Error(data.message || "Request failed");
-        }
-        return result;
-    } catch (error) {
-        throw new Error(error.message || "Network error");
-    }
-}
+//         if (!response.ok) {
+//             throw new Error(data.message || "Request failed");
+//         }
+//         return result;
+//     } catch (error) {
+//         throw new Error(error.message || "Network error");
+//     }
+// }
 
-export const get = requester.bind(null, "GET");
-export const post = requester.bind(null, "POST");
-export const put = requester.bind(null, "PUT");
-export const del = requester.bind(null, "DELETE");
+// export const get = requester.bind(null, "GET");
+// export const post = requester.bind(null, "POST");
+// export const put = requester.bind(null, "PUT");
+// export const del = requester.bind(null, "DELETE");
 
-export default {
-    get,
-    post,
-    put,
-    del,
-};
+// export default {
+//     get,
+//     post,
+//     put,
+//     del,
+// };
