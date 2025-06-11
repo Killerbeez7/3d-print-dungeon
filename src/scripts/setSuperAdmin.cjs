53a3d6bbe778cd96eab9136a8d7755f0e20332d9
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-06-11 16:54:06 +0300

    fix  key import

--- src/scripts/setSuperAdmin.cjs
+++ src/scripts/setSuperAdmin.cjs
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./key.json");
+const serviceAccount = require("../keys/service_key.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
