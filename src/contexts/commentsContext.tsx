bb29648dabb70233584fc133681ec2847653ad8f
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-07-04 19:30:20 +0300

    Refactor[commentsContext]: Add typescript

--- src/contexts/commentsContext.tsx
+++ src/contexts/commentsContext.tsx
@@ -1,10 +1,25 @@
 import { createContext } from "react";
 
-export const CommentsContext = createContext({
+export interface Comment {
+    id: string;
+    text: string;
+    userId: string;
+    userName: string;
+    createdAt?: { toDate: () => Date };
+}
+
+export interface CommentsContextType {
+    comments: Comment[];
+    loading: boolean;
+    submitComment: (commentData: Omit<Comment, "id" | "createdAt">) => Promise<void>;
+    removeComment: (commentId: string) => Promise<void>;
+    updateComment: (commentId: string, newData: Partial<Pick<Comment, "text">>) => Promise<void>;
+}
+
+export const CommentsContext = createContext<CommentsContextType>({
     comments: [],
     loading: false,
-    submitComment: () => {},
-    removeComment: () => {},
-    updateComment: () => {},
-    fetchComments: () => {},
+    submitComment: async () => Promise.resolve(),
+    removeComment: async () => Promise.resolve(),
+    updateComment: async () => Promise.resolve(),
 }); 
