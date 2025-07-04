c5eb0b384e379b66d6799942f3062f30a5259a1c
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-07-04 19:28:24 +0300

    Refactor[Layout]:  remove React.FC

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 
+
 //components
 import { Navbar } from "./navbar/Navbar";
 import { Footer } from "./footer/Footer";
@@ -8,26 +9,26 @@ import { AuthModal } from "./auth-modal/AuthModal";
 import { Spinner } from "@/components/shared/Spinner";
 import { ScrollToTopButton } from "@/components/shared/ScrollToTopButton";
 
-export const Layout: React.FC = () => (
-    <div className="flex flex-col min-h-screen">
-        <Navbar />
-
-        <main className="flex-1">
-            <Suspense
-                fallback={
-                    <div className="h-full flex items-center justify-center">
-                        <Spinner size={48} />
-                    </div>
-                }
-            >
-                <Outlet />
-                <ScrollToTopButton />
-            </Suspense>
-        </main>
-
-        <AuthModal />
-        <Footer />
-    </div>
-);
+export const Layout = () => {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <main className="flex-1">
+                <Suspense
+                    fallback={
+                        <div className="h-full flex items-center justify-center">
+                            <Spinner size={48} />
+                        </div>
+                    }
+                >
+                    <Outlet />
+                    <ScrollToTopButton />
+                </Suspense>
+            </main>
+            <AuthModal />
+            <Footer />
+        </div>
+    );
+};
 
 export default Layout;
