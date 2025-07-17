99277c675449c8b42d0740f1e65d17651cef2842
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-07-17 22:31:56 +0300

    refactor(thumbnalSizes.ts): export ThumbKey

--- src/constants/thumbnailSizes.ts
+++ src/constants/thumbnailSizes.ts
@@ -1,6 +1,8 @@
 export const THUMBNAIL_SIZES = {
-    SMALL: "200x200",
-    MEDIUM: "400x400",
-    LARGE: "800x800",
-    XLARGE: "1200x1200",
-};
+  SMALL:  "200x200",
+  MEDIUM: "400x400",
+  LARGE:  "800x800",
+  XLARGE: "1200x1200",
+} as const;
+
+export type ThumbKey = keyof typeof THUMBNAIL_SIZES;
