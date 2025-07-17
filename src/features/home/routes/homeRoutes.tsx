0376d19e0415e591b0a9a80aeef46d5288b06e9f
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2025-07-17 22:35:01 +0300

    refactor(HomeRoutes): rename Home -> HomePage

--- src/features/home/routes/homeRoutes.tsx
+++ src/features/home/routes/homeRoutes.tsx
@@ -1,9 +1,9 @@
-import { Home } from "@/features/home/components/Home";
+import { HomePage } from "@/features/home/pages/HomePage";
 
 import { withMaintenance } from "@/helpers/routeHelpers";
 import { ROUTES } from "@/constants/routeConstants";
 import type { RouteObject } from "react-router-dom";
 
 export const homeRoutes: RouteObject[] = [
-    { path: ROUTES.HOME, element: withMaintenance(<Home />) },
+    { path: ROUTES.HOME, element: withMaintenance(<HomePage />) },
 ];
