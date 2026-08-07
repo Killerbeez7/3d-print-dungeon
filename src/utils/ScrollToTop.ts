3e355073670a52110d3295044f7ed6a5076d2648
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2026-08-07 11:39:52 +0300

    remove "React.FC" and add behavior: "auto"

--- src/utils/ScrollToTop.ts
+++ src/utils/ScrollToTop.ts
@@ -1,13 +1,16 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+export const ScrollToTop = () => {
+  const { pathname } = useLocation();
 
-export const ScrollToTop: React.FC = () => {
-    const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "auto",
+    });
+  }, [pathname]);
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [pathname]);
-
-    return null;
+  return null;
 };
