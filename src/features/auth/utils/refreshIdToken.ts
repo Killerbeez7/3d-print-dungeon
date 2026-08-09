7eb2a04c8b0d5f3776d89888a38eaa1407dd76e7
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2026-08-09 22:23:51 +0300

    simplify auth token refresh

--- src/features/auth/utils/refreshIdToken.ts
+++ src/features/auth/utils/refreshIdToken.ts
@@ -1,9 +1,13 @@
 import { auth } from "@/config/firebaseConfig";
 
 export const refreshIdToken = async () => {
-    const user = auth.currentUser;
-    if (!user) throw new Error("No user is currently signed in");
-    await user.getIdToken(true);
-    const result = await user.getIdTokenResult(true);
-    return result.claims;
+  const user = auth.currentUser;
+
+  if (!user) {
+    throw new Error("No user is currently signed in");
+  }
+
+  const tokenResult = await user.getIdTokenResult(true);
+
+  return tokenResult.claims;
 };
