f338de756905efed793845f603b7c60667e2f5d6
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2026-08-09 22:35:56 +0300

    format

--- src/features/auth/hooks/useUserRole.ts
+++ src/features/auth/hooks/useUserRole.ts
@@ -1,12 +1,10 @@
 import type { Role } from "@/features/user/types/user";
-import { useAuth } from "../hooks/useAuth";
+import { useAuth } from "./useAuth";
 
 export function useUserRole() {
   const { roles, isAdmin } = useAuth();
 
-  const hasRole = (role: Role) =>
-    roles.includes(role) ||
-    (role === "admin" && isAdmin);
+  const hasRole = (role: Role) => roles.includes(role) || (role === "admin" && isAdmin);
 
   return {
     roles,
