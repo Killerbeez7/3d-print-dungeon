068a59c5e9160a65b78f5d46d71c124b33c15bee
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2026-08-11 14:19:36 +0300

    refactor(routes): remove legacy maintenance routing

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,9 +2,8 @@ import { Navigate, useRoutes, type RouteObject } from "react-router-dom";
 
 import { ROUTES } from "./constants/routeConstants";
 
-// import { authRoutes } from "./features/auth/routes/authRoutes";
-import { userRoutes } from "./features/user/routes/userRoutes";
 import { homeRoutes } from "./features/home/routes/homeRoutes";
+import { userRoutes } from "./features/user/routes/userRoutes";
 import { blogRoutes } from "./features/blog/routes/blogRoutes";
 import { adminRoutes } from "./features/admin/routes/adminRoutes";
 import { forumRoutes } from "./features/forum/routes/forumRoutes";
@@ -15,7 +14,6 @@ import { artistsRoutes } from "./features/artists/routes/artistsRoutes";
 import { policiesRoutes } from "./features/cookies/routes/policiesRoutes";
 import { businessRoutes } from "./features/business/routes/businessRoutes";
 import { collectionsRoutes } from "./features/collections/routes/collectionsRoutes";
-// import { maintenanceRoutes } from "./features/maintenance/routes/maintenanceRoutes";
 import { marketplaceRoutes } from "./features/marketplace/routes/marketplaceRoutes";
 import { printedFiguresRoutes } from "./features/printed-figures/routes/printedFiguresRoutes";
 
@@ -25,21 +23,19 @@ import { MaintenanceRoute } from "@/routes/guards/MaintenanceRoute";
 
 const mainRoutes: RouteObject[] = [
   ...homeRoutes,
-  // ...maintenanceRoutes,
-  // ...authRoutes,
-  ...collectionsRoutes,
-  ...artistsRoutes,
-  ...eventsRoutes,
-  ...blogRoutes,
-  ...marketplaceRoutes,
-  ...printedFiguresRoutes,
   ...userRoutes,
-  ...searchRoutes,
-  ...modelsRoutes,
-  ...businessRoutes,
+  ...blogRoutes,
   ...adminRoutes,
   ...forumRoutes,
+  ...eventsRoutes,
+  ...modelsRoutes,
+  ...searchRoutes,
+  ...artistsRoutes,
   ...policiesRoutes,
+  ...businessRoutes,
+  ...collectionsRoutes,
+  ...marketplaceRoutes,
+  ...printedFiguresRoutes,
   { path: "*", element: <Navigate to={ROUTES.HOME} replace /> },
 ];
 
@@ -57,13 +53,6 @@ const routesConfig: RouteObject[] = [
     children: mainRoutes,
   },
 ];
-
-// const routesConfig: RouteObject[] = [
-//   {
-//     element: <Layout />,
-//     children: mainRoutes,
-//   },
-// ];
 
 export const AppRoutes = () => {
   return useRoutes(routesConfig);
--- src/features/maintenance/routes/maintenanceRoutes.tsx
+++
@@ -1,8 +0,0 @@
-import { MaintenancePage } from "../pages/MaintenancePage";
-
-import { ROUTES } from "../../../constants/routeConstants";
-import type { RouteObject } from "react-router-dom";
-
-export const maintenanceRoutes: RouteObject[] = [
-    { path: ROUTES.MAINTENANCE, element: <MaintenancePage /> },
-];
