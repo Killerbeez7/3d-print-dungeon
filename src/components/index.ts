fff070c7dcb6856b43596f5ef85923409fee2491
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2026-08-14 21:47:17 +0300

    refactor(Button): add clsx

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,45 +1,38 @@
 // Re-export paragraph components for convenience
-export {
-    Button,
-    buttonStyles,
-} from './Button';
+export { Button } from "./Button";
 
-export type {
-    ButtonProps,
-    ButtonSize,
-    ButtonVariant,
-} from './Button';
+export type { ButtonProps, ButtonSize, ButtonVariant } from "./Button";
 
 export {
-    H1,
-    H2,
-    H3,
-    H4,
-    H5,
-    H6,
-    ResponsiveHeading,
-    PageTitle,
-    SectionTitle,
-    SubsectionTitle,
-    CompactHeading,
-    HeroHeading
-} from './ResponsiveHeading';
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  ResponsiveHeading,
+  PageTitle,
+  SectionTitle,
+  SubsectionTitle,
+  CompactHeading,
+  HeroHeading,
+} from "./ResponsiveHeading";
 
 export {
-    ResponsiveParagraph,
-    ParagraphLarge,
-    ParagraphSmall,
-    ParagraphExtraSmall,
-    ParagraphBase,
-    Caption,
-    Label,
-    Description,
-    Metadata,
-    StatValue,
-    StatValueSecondary,
-    Badge,
-    BadgeSmall,
-    TabLabel,
-    FormLabel,
-    HelpText
-} from './ResponsiveParagraph';
+  ResponsiveParagraph,
+  ParagraphLarge,
+  ParagraphSmall,
+  ParagraphExtraSmall,
+  ParagraphBase,
+  Caption,
+  Label,
+  Description,
+  Metadata,
+  StatValue,
+  StatValueSecondary,
+  Badge,
+  BadgeSmall,
+  TabLabel,
+  FormLabel,
+  HelpText,
+} from "./ResponsiveParagraph";
